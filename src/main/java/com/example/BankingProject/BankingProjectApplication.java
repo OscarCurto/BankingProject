26a3b29798762c9672dbf6d00190c87610ba9d78
Oscar Curto <oscarcj98@gmail.com>
2022-10-21 10:33:45 +0200

    Before deleting non used classes

--- src/main/java/com/example/BankingProject/BankingProjectApplication.java
+++ src/main/java/com/example/BankingProject/BankingProjectApplication.java
@@ -119,6 +119,6 @@ public class BankingProjectApplication implements CommandLineRunner {
         SpringApplication.run(BankingProjectApplication.class, args);
     }
 
-    //TODO COLLECTION POSTMAN, README, UMLS, LIMPIAR
+    //TODO README, UMLS, LIMPIAR
 
 }
