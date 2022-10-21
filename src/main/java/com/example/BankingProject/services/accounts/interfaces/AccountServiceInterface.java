ce8114c116d408326fd97b2a7d46553b6021914d
Oscar Curto <oscarcj98@gmail.com>
2022-10-21 10:41:15 +0200

    Deleted non used services from account

--- src/main/java/com/example/BankingProject/services/accounts/interfaces/AccountServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.BankingProject.services.accounts.interfaces;
-
-import com.example.BankingProject.models.accounts.Account;
-
-import java.util.List;
-
-public interface AccountServiceInterface {
-
-}
--- src/main/java/com/example/BankingProject/services/accounts/interfaces/StudentCheckingAccountServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.BankingProject.services.accounts.interfaces;
-
-import com.example.BankingProject.models.accounts.StudentCheckingAccount;
-
-import java.util.List;
-
-public interface StudentCheckingAccountServiceInterface {
-
-}
